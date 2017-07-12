725653c711b29f2c571d9872a37130d3517d0371
corey redding and daniela trulls <corey+daniela@>
2017-07-12 11:51:55 -0700

    follow lessons and add admin component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent }   from './album-detail/album-detail.component';
+import { AdminComponent }   from './admin/admin.component';
 
 
 
@@ -18,6 +20,14 @@ const appRoutes: Routes = [
   {
     path: 'marketplace',
     component: MarketplaceComponent
+  },
+  {
+    path: 'albums/:id',
+    component: AlbumDetailComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
   }
 ];
 
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
+import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
+  constructor(private router: Router, private albumService: AlbumService) { }
 
-  constructor() { }
+  albums: FirebaseListObservable<any[]>;;
+
+  goToDetailPage(clickedAlbum: Album) {
+    this.router.navigate(['albums', clickedAlbum.id]);
+  };
 
   ngOnInit() {
+    this.albums = this.albumService.getAlbums();
   }
 
 }
